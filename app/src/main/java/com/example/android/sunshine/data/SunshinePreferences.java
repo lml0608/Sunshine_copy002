5309629e620461d670257edab16cc690d963ef8a
liubin001 <183773928@qq.com>
2017-05-05 17:37:32 +0800

    jobservice

--- app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -169,4 +169,30 @@ public class SunshinePreferences {
         /** This will be implemented in a future lesson **/
         return DEFAULT_WEATHER_COORDINATES;
     }
+
+
+
+    public static boolean areNotificationsEnabled(Context context) {
+
+        String displayNotificationsKey = context.getString(R.string.pref_enable_notifications_key);
+
+        boolean shouldDisplayNotificationsByDefault = context
+                .getResources()
+                .getBoolean(R.bool.show_notifications_by_default);
+
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean shouldDisplayNotifications = sp
+                .getBoolean(displayNotificationsKey, shouldDisplayNotificationsByDefault);
+
+        return shouldDisplayNotifications;
+    }
+
+
+    public static void saveLastNotificationTime(Context context, long timeOfNotification) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sp.edit();
+        String lastNotificationKey = context.getString(R.string.pref_last_notification);
+        editor.putLong(lastNotificationKey, timeOfNotification);
+        editor.apply();
+    }
 }
